03be22a831b4ad19c9022471c1a06ee65348a07c
Guy <premsupapong@gmail.com>
2020-07-03 09:54:49 +0700

    successfully set up inital wrapper testing

--- test/test.cpp
+++ test/test.cpp
@@ -1,12 +1,12 @@
 #include "test.h"
 
 // NOTE: <SDL2/SDL.h> alrdy included in the wrapper files
-#include "lib/sdl.h"
-#include "lib/sdl_window.h"
+#include "../lib/sdl.h"
+#include "../lib/sdl_window.h"
 
 int main(){
 
     // initalise SDL env
-
+    SDLwrap::SDL sdl = SDLwrap::SDL();
 
 }
