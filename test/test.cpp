d69ac8dd55d2ec1bf9e8fb94e2835fc112c7da9a
Guy <premsupapong@gmail.com>
2020-07-04 11:32:43 +0700

    try-catch in test file

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,25 @@
-#include "test.h"
+#include <iostream>
 
 // NOTE: <SDL2/SDL.h> alrdy included in the wrapper files
 #include "../lib/sdl.h"
+#include "../lib/sdl_exceptions.h"
 #include "../lib/sdl_window.h"
 
-int main(){
+#include "test.h"
 
-    // initalise SDL env
-    SDLwrap::SDL sdl = SDLwrap::SDL();
+int main(){ try{
 
-    // initalise window wrapper object
-    SDLwrap::Window win = SDLwrap::Window();
+        // initalise SDL env
+        SDLwrap::SDL sdl = SDLwrap::SDL();
+
+        // initalise window wrapper object
+        SDLwrap::Window win = SDLwrap::Window();
+
+    }catch(SDLwrap::Exception e){
+
+        std::cout << "SDL Error occured from: " << e.get_fnc() << std::endl;
+        std::cout << "SDL Error: " << e.get_err() << std::endl;
+
+    }
 
 }
