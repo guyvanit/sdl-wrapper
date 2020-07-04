98110695db4c439cabe9893e03b9da1280ec3b1c
Guy <premsupapong@gmail.com>
2020-07-04 11:11:39 +0700

    added example wrapper usage

--- lib/sdl.cpp
+++ lib/sdl.cpp
@@ -3,6 +3,26 @@
 #include "sdl.h"
 #include "sdl_exceptions.h"
 
+/*
+    Example initialisation by:
+
+    #include "sdl.h"
+    #include "sdl_exceptions.h"
+
+    try{
+
+        SDLwrap::SDL sdl = SDLwrap::SDL();
+        // do some SDL stuff
+
+    }catch(SDLwrap::Exception e){
+
+        std::cout << "Error occured from " << e.get_fnc() << std::endl;
+        std::cout << "SDL Error: " <, e.get_err() << std::endl;
+
+    }
+
+*/
+
 namespace SDLwrap{
 
 SDL::SDL(Uint32 flags){
