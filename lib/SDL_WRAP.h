aa3e968b6b10b2aba88e418cd289e664d9806a24
Guy <premsupapong@gmail.com>
2020-07-09 13:57:47 +0700

    commented texture as still WIP

--- lib/SDL_WRAP.h
+++ lib/SDL_WRAP.h
@@ -6,10 +6,10 @@
 #include "sdl.h"
 #include "sdl_window.h"
 #include "sdl_renderer.h"
-#include "sdl_texture.h"
 
 // ---------- SDL_IMG ----------
 #include "sdl_img.h"
+// #include "sdl_texture.h"
 
 // ------ general headers ------
 #include "sdl_exceptions.h"
