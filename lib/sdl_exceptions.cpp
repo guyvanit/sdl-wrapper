7ff9155a5de83f46e67004e8aa71df5e29d4d5a2
Guy <premsupapong@gmail.com>
2020-07-09 15:02:38 +0700

    minor refactor

--- lib/sdl_exceptions.cpp
+++ lib/sdl_exceptions.cpp
@@ -3,14 +3,15 @@
 
 namespace SDLwrap{
 
-std::string Exception::make_msg(std:: string fnc, std::string err){
-    return "SDL Error at " + fnc + " : " + err;
-}
-
 Exception::Exception(std::string f)
     : std::runtime_error(make_msg(f, SDL_GetError())){
     fnc_ = f;
     sdl_err_ = SDL_GetError();
+}
+
+// TODO? : Change to pass-by-ref if possible to save memory
+std::string Exception::make_msg(std::string fnc, std::string err){
+    return "SDL Error at " + fnc + " : " + err;
 }
 
 std::string Exception::get_err(){
--- lib/sdl_exceptions.h
+++ lib/sdl_exceptions.h
@@ -10,11 +10,10 @@ class Exception : public std::runtime_error {
 
     // stores error message from SDL_Error()
     std::string sdl_err_;
-
     // stores which function causes the exception to be thrown
     std::string fnc_;
 
-    std::string make_msg(std:: string fnc, std::string err);
+    std::string make_msg(std::string fnc, std::string err);
 
     public:
         // constructor + (default) destructor?
