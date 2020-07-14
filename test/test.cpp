dcc8ac2a549ab4995c57f9f3862f7092036bd376
Guy <premsupapong@gmail.com>
2020-07-14 15:12:11 +0700

    commented tests

--- test/test.cpp
+++ test/test.cpp
@@ -7,32 +7,38 @@ int main(){ try{
 
         // test SDL env
         SDLwrap::SDL sdl = SDLwrap::SDL();
+        // std::cout << "SDL: " << &sdl << std::endl;
 
         //test SDL_IMG
         SDLwrap::SDL_IMG sdl_img = SDLwrap::SDL_IMG();
+        // std::cout << "SDL_IMG: " << &sdl_img << std::endl;
 
         // test window wrapper object
         SDLwrap::Window win = SDLwrap::Window();
+        // std::cout << "Window: " << &win << std::endl;
 
         // test renderer wrapper object
         SDLwrap::Renderer ren = SDLwrap::Renderer(win);
+        // std::cout << "Renderer: " << &ren << std::endl;
         ren.setDrawColor();
         ren.clear();
         ren.update();
 
         // test surface wrapper
         SDLwrap::Surface surf = SDLwrap::Surface();
+        // std::cout << "Surface: " << &surf << std::endl;
         std::string fname = "test_img.jpg";
         surf.loadFile(fname);
         // std::cout << surf.get_surface() << std::endl;
 
         // test texture wrapper
         SDLwrap::Texture tex = SDLwrap::Texture();
+        // std::cout << "Texture: " << &tex << std::endl;
         tex.loadFile(ren, fname);
 
         // test texture rendering
         tex.fit_window = &win;
-        std::cout << tex.fit_window << std::endl;
+        // std::cout << tex.fit_window << std::endl;
         // std::cout << win.get_width() << " : " << win.get_height() << std::endl;
         tex.render(ren);
 
