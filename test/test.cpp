bb92a3564cf07d38f31c18eb207ae360e4e0cb37
Guy <premsupapong@gmail.com>
2020-08-11 13:19:17 +0700

    restructure and added gl test

--- test/test.cpp
+++ test/test.cpp
@@ -8,48 +8,47 @@
 
 int main(){ try{
 
-        // test SDL env
+        // ------ SDL TEST ------
+
         SDLwrap::SDL sdl = SDLwrap::SDL();
         sdl.printVer();
-        // std::cout << "SDL: " << &sdl << std::endl;
 
-        //test SDL_IMG
+        // ------ SDL_IMG TEST ------
+
         SDLwrap::SDL_IMG sdl_img = SDLwrap::SDL_IMG();
-        // std::cout << "SDL_IMG: " << &sdl_img << std::endl;
 
-        // test window wrapper object
+        // ------ WINDOW TEST ------
+
         SDLwrap::Window win = SDLwrap::Window();
         win.set_GLContext();
         SDL_GLContext cont = win.get_GLContext();
         std::cout << "cont: " << cont << std::endl;
-        // std::cout << "Window: " << &win << std::endl;
+        win.updateGL();
 
-        // test renderer wrapper object
+        // ------ RENDERER TEST ------
+
         SDLwrap::Renderer ren = SDLwrap::Renderer(win);
-        // std::cout << "Renderer: " << &ren << std::endl;
         ren.setDrawColor();
         ren.clear();
         ren.update();
 
-        // test surface wrapper
+        // ------ SURFACE TEST ------
+
         SDLwrap::Surface surf = SDLwrap::Surface();
-        // std::cout << "Surface: " << &surf << std::endl;
         std::string fname = "test_img.jpg";
         surf.loadFile(fname);
-        // std::cout << surf.get_surface() << std::endl;
+
+        // ------ (MORE) SDL TEST ------
 
         sdl.specGL(3,3,true);
         sdl.setVSync(1);
 
-        // test texture wrapper
+        // ------ TEXTURE TEST ------
+
         SDLwrap::Texture tex = SDLwrap::Texture();
-        // std::cout << "Texture: " << &tex << std::endl;
         tex.loadFile(ren, fname);
 
-        // test texture rendering
         tex.fit_window = &win;
-        // std::cout << tex.fit_window << std::endl;
-        // std::cout << win.get_width() << " : " << win.get_height() << std::endl;
         tex.render(ren);
 
     }catch(SDLwrap::Exception &e){
